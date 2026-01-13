ff2b0422fa8fbd02d2f0089b2c87b22788aa3749
redjon.varfi <redjon.varfi@advantagesolutions.net>
2026-01-13 14:38:50 +0100

    test env

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -11,7 +11,7 @@ module "platform" {
   vpc_cidr           = var.vpc_cidr
   enable_nat_gateway = var.enable_nat_gateway
   instance_types     = var.instance_types
-  node_group = var.node_group
+  node_group         = var.node_group
 
   cluster_endpoint_public_access  = var.cluster_endpoint_public_access
   cluster_endpoint_private_access = var.cluster_endpoint_private_access
